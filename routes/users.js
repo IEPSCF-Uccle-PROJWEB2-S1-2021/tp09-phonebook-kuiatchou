36a0611e9bf69e20b58db58c52b83f45f9e9b4d4
kuiatchou merveille <kume0804@student.iepscf-uccle.be>
2020-11-30 23:55:26 +0100

    encours

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = new express.Router();
 
+function user(lastName,firstName,BirthDate,phoneNumber,emailAddress){
+  this.lastName =lastName;
+  this.firstName=firstName;
+  this.BirthDate=BirthDate;
+  this.phoneNumber=phoneNumber;
+  this.emailAddress=emailAddress;
+}
+const users = [];
+
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
+router.post('/',(req,res,next)=>{
+  const user = new user(req.body.lastName, req.body.firstName, req.body.BirthDate,
+    req.body.phoneNumber, req.body.emailAddress);
+  users.push(user);
+  res.redirect('/');
 
+});
 module.exports = router;
